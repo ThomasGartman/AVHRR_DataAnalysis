0da4ee584cc41b1da6fec50604502cff779a103e
ThomasGartman <thomasgartman@ku.edu>
2018-10-01 13:54:07 -0500

    Now able to manipulate the NDVI data for all 27 years of data.

--- scripts/MatToCSV.m
+++ scripts/MatToCSV.m
@@ -3,18 +3,24 @@
 % Reuman Lab
 % This file allows for an exploration of the data and autodoc systems. It
 % will eventually be converted to a mat to CSV reader for R
-% Last Edited: September 26th, 2018
+% Last Edited: October 1st, 2018
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 clc;
 clear all;
 
 %load in datafiles
 %load in location information, lamber projection
+disp('Loading data....')
 load('data/mat/avhrr_1km_map_coords.mat') %lamber projection
 load('data/mat/avhrr_1km_pixel_latlon.mat') 
 load('data/mat/avhrr_vpm_1989_2015_mxvi.mat') %NDVI
 load('data/mat/avhrr_vpm_1989_2015_mxdt.mat') %Growing Season
 load('data/mat/avhrr_cover_frac_nlcd2011.mat') %Land Cover codes. Using to get water
+disp('Data Loaded')
+
+%setup colormap
+alteredJet = jet;
+alteredJet(64,:) = 0;
 
 %Rename variables
 xlam = avhrr_lamaz_map_coord_x;
@@ -62,31 +68,32 @@ clear nlcd_cls_frac;
 waterLocations = reshape(waterLocations, [4587, 2889]);
 waterLocations = transpose(waterLocations);
 
-%Make figure of NDVI in the united states for the year 1989
-ndvi = mxvi(:,1);
-clear mxvi;
-ndvi = reshape(ndvi, [4587,2889]);
-ndvi = transpose(ndvi);
+figNDVI = gobjects(27, 1);
+for k = 1:27
+    disp(strcat(string('Figure '), int2str(k), string(' of '), int2str(27)))
+    %Make figure of NDVI in the united states for all years
+    ndvi = mxvi(:,k);
+    ndvi = reshape(ndvi, [4587,2889]);
+    ndvi = transpose(ndvi);
 
-%Alter NDVI matrix by removing water/NonUS pixels
-for i = 1:2889
-    for j = 1:4587
-        if(waterLocations(i, j) == 1 || waterLocations(i, j) == 2)
-            ndvi(i, j) = 2;
+    %Alter NDVI matrix by removing water/NonUS pixels
+    for i = 1:2889
+        for j = 1:4587
+            if(waterLocations(i, j) == 1 || waterLocations(i, j) == 2)
+                ndvi(i, j) = 2;
+            end
         end
     end
+    
+    figNDVI(k) = figureImageScaler(ndvi);
+    set(figNDVI(k), 'Name', ['Normalized Difference Vegetation Index for ' int2str(1988 + k)], 'NumberTitle','off');
+    title(['Normalized Difference Vegetation Index for '  int2str(1988 + k)]);
+    caxis([0 1]);
+    colormap(alteredJet);
 end
+clear mxvi;
 
-%setup colormap
-alteredJet = jet;
-alteredJet(64,:) = 0;
-
-figNDVI = figureImageScaler(ndvi);
-set(figNDVI, 'Name', 'Normalized Difference Vegetation Index for 1989', 'NumberTitle','off');
-title('Normalized Difference Vegetation Index for 1989');
-caxis([0 1]);
-colormap(alteredJet);
-
+%TODO Find updated autodoc
 warning('off', 'MATLAB:nargchk:deprecated'); %Apparently this system is using a deperciated function...
 m2html('mfiles', 'scripts' ,'htmldir', 'docs', 'recursive', 'on', 'global', 'on');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -106,5 +113,4 @@ function f = figureImageScaler(data)
     f = figure;
     imagesc(data);
     colormap('jet');
-    colorbar;
 end
