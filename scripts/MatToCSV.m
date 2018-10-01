11480a0a8170ddd2a162ec73766b521a13ca3c9e
ThomasGartman <thomasgartman@ku.edu>
2018-10-01 15:16:42 -0500

    Documentation Updated!

--- scripts/MatToCSV.m
+++ scripts/MatToCSV.m
@@ -1,19 +1,47 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Thomas Gartman
-% Reuman Lab
-% This file allows for an exploration of the data and autodoc systems. It
-% will eventually be converted to a mat to CSV reader for R
-% Last Edited: October 1st, 2018
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%MatToCSV in AVHRRGeographyOfSpatialSynchronyReproduce
+%Version 1.0.0  Last Editied October 1st, 2018
+%
+%Takes in matlab files for AVHRR Data and converts them to csv files
+%
+%Preconditions:
+%   1. .mat files for the AVHRR data are located in a data folder which is 
+%   not in the same folder as this script
+%
+%   2. This file must be located in a script folder which is in the same
+%   project folder as the data folder
+%
+%   3. The m2html folder must be in the project path and located in the
+%   same project folder as the script and data folders
+%
+%Postconditions:
+%   .csv files for AVHRR data will be generated in data/csvFiles
+%
+%Data processing:
+%   NDVI data for each year is reshaped into US shape, then pixel locations
+%   for the water or nodata are logged with a value of 2, which is higher
+%   than the maximum NDVI.
+%
+%Structure of imported data:
+%   avhrr_vpm_1989_2015_mxvi.mat contains the NDVI data, called mxvi, is a
+%   2D array with dimensions 13251843x27, where the first dimension is the
+%   data and the second is the year, starting at 1989 and ending at 2015.
+%   Each data index represents a pixel on the map of the united states with
+%   dimension 4587 by 2889. Each pixel is 1km by 1km and each data point is
+%   the maximum NDVI data for that year in that pixel.
+%
+%   avhrr_cover_frac_nlcd2011.mat contains land cover codes. The codes are
+%   0 for land, 1 for water, and 2 for no data (where no data means land
+%   outside of the United States). The structure is a vector of dimension
+%   13251843, where each index represents a land cover code for a 1km by
+%   1km pixel.
+
 clc;
 clear all;
 
 %load in datafiles
 %load in location information, lamber projection
-disp('Loading data....')
 load('data/mat/avhrr_vpm_1989_2015_mxvi.mat') %NDVI
 load('data/mat/avhrr_cover_frac_nlcd2011.mat') %Land Cover codes. Using to get water
-disp('Data Loaded')
 
 %create matrix of water pixels
 waterLocations = nlcd_cls_frac(:,1);
@@ -22,8 +50,6 @@ waterLocations = reshape(waterLocations, [4587, 2889]);
 waterLocations = transpose(waterLocations);
 
 for k = 1:27
-    disp(strcat(string('Year '), int2str(k), string(' of '), int2str(27)))
-    %Make figure of NDVI in the united states for all years
     ndvi = mxvi(:,k);
     ndvi = reshape(ndvi, [4587,2889]);
     ndvi = transpose(ndvi);
@@ -36,6 +62,8 @@ for k = 1:27
             end
         end
     end
+    
+    %make CSV Files
     fileName = char(strcat(string('AVHRR_NDVI_WaterRemoved_'), int2str(k + 1988), string('.csv')));
     csvwrite(fileName,ndvi);
     movefile *.csv data/csvFiles/;
@@ -45,21 +73,3 @@ clear mxvi;
 %TODO Find updated autodoc
 warning('off', 'MATLAB:nargchk:deprecated'); %Apparently this system is using a deperciated function...
 m2html('mfiles', 'scripts' ,'htmldir', 'docs', 'recursive', 'on', 'global', 'on');
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Since this function is temporary, the full matlab documentation structure 
-% will not be implemented to document figureImageScaler
-%
-% This function's purpose is to generate multiple figures without declaring
-% multiple figures in the script itself
-%
-% Args:
-%   data - a vector of data to form an image from
-%
-% Returns:
-%   Figure of data image
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function f = figureImageScaler(data)
-    f = figure;
-    imagesc(data);
-    colormap('jet');
-end
--- scripts/NDVIFigureGenerator.m
+++ scripts/NDVIFigureGenerator.m
@@ -1,66 +1,46 @@
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Thomas Gartman
-% Reuman Lab
-% This file allows for an exploration of the data and autodoc systems. It
-% will eventually be converted to a mat to CSV reader for R
-% Last Edited: October 1st, 2018
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%NDVIFigureGenerator in AVHRRGeographyOfSpatialSynchronyReproduce
+%Version 1.0.0  Last Editied October 1st, 2018
+%
+%Takes in matlab files for AVHRR Data and creates figures to visual data
+%
+%Preconditions:
+%   1. .mat files for the AVHRR data are located in a data folder which is 
+%   not in the same folder as this script
+%
+%   2. This file must be located in a script folder which is in the same
+%   project folder as the data folder
+%
+%   3. The m2html folder must be in the project path and located in the
+%   same project folder as the script and data folders
+%
+%Postconditions:
+%   A .gif file will be created in the project folder.
+%
+%Data processing:
+%   NDVI data for each year is reshaped into US shape, then pixel locations
+%   for the water or nodata are logged with a value of 2, which is higher
+%   than the maximum NDVI.
+%
+%Structure of imported data:
+%   avhrr_vpm_1989_2015_mxvi.mat contains the NDVI data, called mxvi, is a
+%   2D array with dimensions 13251843x27, where the first dimension is the
+%   data and the second is the year, starting at 1989 and ending at 2015.
+%   Each data index represents a pixel on the map of the united states with
+%   dimension 4587 by 2889. Each pixel is 1km by 1km and each data point is
+%   the maximum NDVI data for that year in that pixel.
+%
+%   avhrr_cover_frac_nlcd2011.mat contains land cover codes. The codes are
+%   0 for land, 1 for water, and 2 for no data (where no data means land
+%   outside of the United States). The structure is a vector of dimension
+%   13251843, where each index represents a land cover code for a 1km by
+%   1km pixel.
+
 clc;
 clear all;
 
 %load in datafiles
-%load in location information, lamber projection
-disp('Loading data....')
-load('data/mat/avhrr_1km_map_coords.mat') %lamber projection
-load('data/mat/avhrr_1km_pixel_latlon.mat') 
 load('data/mat/avhrr_vpm_1989_2015_mxvi.mat') %NDVI
-load('data/mat/avhrr_vpm_1989_2015_mxdt.mat') %Growing Season
 load('data/mat/avhrr_cover_frac_nlcd2011.mat') %Land Cover codes. Using to get water
-disp('Data Loaded')
-
-%setup colormap
-alteredJet = jet;
-alteredJet(64,:) = 0;
-
-%Rename variables
-xlam = avhrr_lamaz_map_coord_x;
-ylam = avhrr_lamaz_map_coord_y;
-clear avhrr_lamaz_map_coord_x;
-clear avhrr_lamaz_map_coord_y;
-
-lat = avhrr_pixel_lat;
-lon = avhrr_pixel_lon;
-clear avhrr_pixel_lat;
-clear avhrr_pixel_lon;
-
-%Reshape to make matrix
-xlam = reshape(xlam, [4587,2889]);
-ylam = reshape(ylam, [2889,4587]);
-
-xlam = transpose(xlam);
-ylam = transpose(ylam);
-
-%Make figures from lamber projection
-figXlam = figureImageScaler(xlam);
-set(figXlam, 'Name', 'X Lamber Projection', 'NumberTitle', 'off');
-title('X Lamber Projection');
-figYlam = figureImageScaler(ylam);
-set(figYlam, 'Name', 'Y Lamber Projection', 'NumberTitle', 'off');
-title('Y Lamber Projection');
-
-%LAT AND LON ARE IN DEGREES 
-lon = reshape(lon, [4587,2889]);
-lat = reshape(lat, [2889,4587]);
-lon = transpose(lon);
-lat = transpose(lat);
-
-%Make figures from pure lat/lon values. Should be skewed
-figLon = figureImageScaler(lon);
-set(figLon, 'Name', 'Longitude', 'NumberTitle','off');
-title('Longitude');
-figLat = figureImageScaler(lat);
-set(figLat, 'Name', 'Latitude', 'NumberTitle','off');
-title('Latitude');
 
 %create matrix of water pixels
 waterLocations = nlcd_cls_frac(:,1);
@@ -68,12 +48,16 @@ clear nlcd_cls_frac;
 waterLocations = reshape(waterLocations, [4587, 2889]);
 waterLocations = transpose(waterLocations);
 
+%Set up altered jet colormap
+alteredJet = jet;
+alteredJet(64,:) = 0;
+
 %Set up gif creation
 axis tight manual;
 fileName = 'NDVI1989To2015.gif';
 figNDVI = gobjects(27, 1);
+
 for k = 1:27
-    disp(strcat(string('Figure '), int2str(k), string(' of '), int2str(27)))
     %Make figure of NDVI in the united states for all years
     ndvi = mxvi(:,k);
     ndvi = reshape(ndvi, [4587,2889]);
@@ -111,11 +95,11 @@ clear mxvi;
 warning('off', 'MATLAB:nargchk:deprecated'); %Apparently this system is using a deperciated function...
 m2html('mfiles', 'scripts' ,'htmldir', 'docs', 'recursive', 'on', 'global', 'on');
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Since this function is temporary, the full matlab documentation structure 
-% will not be implemented to document figureImageScaler
+%figureImageScalar in AVHRRGeographyOfSpatialSynchronyReproduce
+%Version 1.0.0 Last Editied October 1st, 2018
 %
-% This function's purpose is to generate multiple figures without declaring
-% multiple figures in the script itself
+% Generates figures without declaring figures in other scripts. Uses jet as
+% a default colormap
 %
 % Args:
 %   data - a vector of data to form an image from
