0f2698c7c55b1e62e2609094814919a691bb8b7f
Thomas Gartman <thomasgartman@ku.edu>
2018-10-18 16:50:24 -0500

    US Map appearing.... it is upside down

--- scripts/NDVIFigureGenerator.R
+++ scripts/NDVIFigureGenerator.R
@@ -6,4 +6,9 @@ library("reshape2")
 #' Create NDVI figures and a gif for the AVHRR NDVI figure
 
 NDVI_1989 <- read.csv("data/csvFiles/AVHRR_NDVI_WaterRemoved_1989.csv", header=FALSE)
-#' NDVIheatmap <- heatmap(NDVI_1989Numeric)
+x = 1:4587
+y = 1:2889
+
+ndviMatrix = data.matrix(NDVI_1989)
+ndviMatrix = t(ndviMatrix)
+NDVIheatmap <- image(x, y, ndviMatrix)
